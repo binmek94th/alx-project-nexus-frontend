5c272117e47265c1f958218a9be1412610b1ce5e
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-08-09 14:44:53 +0300

    feat: add logout button

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import PrimaryButton from "../components/PrimaryButton.tsx";
 import UpdateAccount from "../features/account/UpdateAccount.tsx";
 import UpdateEmail from "../features/account/UpdateEmail.tsx";
 import UpdatePassword from "../features/account/UpdatePassword.tsx";
 
 const Account = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        localStorage.removeItem("user");
+        navigate("/login");
+    }
     return (
-        <div className={'flex flex-col gap-8 pl-15 pt-10'}>
+        <div className={'flex relative min-w-100 flex-col gap-8 pl-15 pt-10'}>
+            <div className={'absolute top-0 left-75 w-full'}>
+                <PrimaryButton onClick={handleLogout}>Logout</PrimaryButton>
+            </div>
             <UpdateAccount></UpdateAccount>
             <UpdateEmail></UpdateEmail>
             <UpdatePassword></UpdatePassword>
